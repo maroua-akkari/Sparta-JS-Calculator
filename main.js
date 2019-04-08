30e66d79ace1982cf353c1261c2925dd47d46153
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2019-04-09 00:33:12 +0100

    added basic calculator

---
+++ main.js
@@ -0,0 +1,36 @@
+//what calc do you want to use? (a) basic (b) advanced
+//ask user for 1st number
+//ask user for operation symbol (a) = +*/- (b) ^square root
+//ask user for 2nd number
+//present back results in an alert
+
+
+var calculatorType = prompt("What calculator do you want to use? (a) basic or (b) advanced");
+
+if (calculatorType.toLowerCase() == "a") {
+  alert("You are now using a basic calculator");
+} else if (calculatorType.toLowerCase() == "b") {
+  alert("You are now using an advanced calculator");
+} else alert("Please enter type of calculator");
+
+var firstNumber = prompt("Enter your first number");
+var operator = prompt("Which operator would you like to use? +/*-");
+var secondNumber = prompt("Enter your second number");
+
+
+switch (operator) {
+  case "+":
+    alert(parseInt(firstNumber)+parseInt(secondNumber));
+    break;
+  case "-":
+    alert(parseInt(firstNumber)-parseInt(secondNumber));
+    break;
+  case "/":
+    alert(parseInt(firstNumber)/parseInt(secondNumber));
+    break;
+  case "*":
+    alert(parseInt(firstNumber)*parseInt(secondNumber));
+    break;
+  default:
+     alert("try again");
+}
