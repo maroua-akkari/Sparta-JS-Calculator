a3e452bc4eb8ff4c4036469873072e794918de90
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2019-04-09 10:59:22 +0100

    added advanced calculator

--- main.js
+++ main.js
@@ -1,47 +1,38 @@
-//what calc do you want to use? (a) basic (b) advanced
-//ask user for 1st number
-//ask user for operation symbol (a) = +*/- (b) ^square root
-//ask user for 2nd number
-//present back results in an alert
-
 
 var calculatorType = prompt("What calculator do you want to use? (a) basic or (b) advanced");
 
 if (calculatorType.toLowerCase() == "a") {
   alert("You are now using a basic calculator");
+  var firstNumber = prompt("Enter your first number");
+  var operator = prompt("Which operator would you like to use? +/*-");
+  var secondNumber = prompt("Enter your second number");
+
+  switch (operator) {
+    case "+":
+      alert(parseInt(firstNumber)+parseInt(secondNumber));
+      break;
+    case "-":
+      alert(parseInt(firstNumber)-parseInt(secondNumber));
+      break;
+    case "/":
+      alert(parseInt(firstNumber)/parseInt(secondNumber));
+      break;
+    case "*":
+      alert(parseInt(firstNumber)*parseInt(secondNumber));
+      break;
+    default:
+       alert("try again");
+  }
+
 } else if (calculatorType.toLowerCase() == "b") {
   alert("You are now using an advanced calculator");
-} else alert("Please enter type of calculator");
-
-// if (calculatorType.toLowerCase() == "b") {
-//   alert(powerOf);
-// } else alert("try again");
-
-
-
-var firstNumber = prompt("Enter your first number");
-var operator = prompt("Which operator would you like to use? +/*-");
-var secondNumber = prompt("Enter your second number");
-
-
-
-
-switch (operator) {
-  case "+":
-    alert(parseInt(firstNumber)+parseInt(secondNumber));
-    break;
-  case "-":
-    alert(parseInt(firstNumber)-parseInt(secondNumber));
-    break;
-  case "/":
-    alert(parseInt(firstNumber)/parseInt(secondNumber));
-    break;
-  case "*":
-    alert(parseInt(firstNumber)*parseInt(secondNumber));
-    break;
-  default:
-     alert("try again");
+  var powerOrSquare = prompt("To calculate the square of a number enter a, to calculate the square root of a number enter b");
+  if (powerOrSquare == "a") {
+    var firstNumber = parseInt(prompt("Enter your first number"));
+    var powerOf = parseInt(prompt("Enter to which power"));
+    var powerOfResult = alert(firstNumber ** powerOf);
+  } else if (powerOrSquare == "b") {
+    var squareRootNumber = parseInt(prompt("Enter the number you would like to square root"));
+    var squareOfNumber = alert(Math.sqrt(squareRootNumber));
+  } else alert("Please enter type of calculator")
 }
-
-// var powerOf = prompt("Enter to which power")
-// var powerOfResult = Math.pow(parseInt(firstNumber), parseInt(secondNumber));
