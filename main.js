9be2754ba046e1fa135101965d19e7b36cf02269
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2019-04-09 11:10:22 +0100

    added user friendly x * b = xb

--- main.js
+++ main.js
@@ -3,22 +3,22 @@ var calculatorType = prompt("What calculator do you want to use? (a) basic or (b
 
 if (calculatorType.toLowerCase() == "a") {
   alert("You are now using a basic calculator");
-  var firstNumber = prompt("Enter your first number");
+  var firstNumber = parseInt(prompt("Enter your first number"));
   var operator = prompt("Which operator would you like to use? +/*-");
-  var secondNumber = prompt("Enter your second number");
+  var secondNumber = parseInt(prompt("Enter your second number"));
 
   switch (operator) {
     case "+":
-      alert(parseInt(firstNumber)+parseInt(secondNumber));
+      alert(firstNumber + " + " + secondNumber + " = " + firstNumber+secondNumber);
       break;
     case "-":
-      alert(parseInt(firstNumber)-parseInt(secondNumber));
+      alert(firstNumber + " - " + secondNumber + " = " + firstNumber-secondNumber);
       break;
     case "/":
-      alert(parseInt(firstNumber)/parseInt(secondNumber));
+      alert(firstNumber + " / " + secondNumber + " = " +firstNumber/secondNumber);
       break;
     case "*":
-      alert(parseInt(firstNumber)*parseInt(secondNumber));
+      alert(firstNumber + " * " + secondNumber + " = " + firstNumber*secondNumber);
       break;
     default:
        alert("try again");
@@ -30,9 +30,9 @@ if (calculatorType.toLowerCase() == "a") {
   if (powerOrSquare == "a") {
     var firstNumber = parseInt(prompt("Enter your first number"));
     var powerOf = parseInt(prompt("Enter to which power"));
-    var powerOfResult = alert(firstNumber ** powerOf);
+    var powerOfResult = alert(firstNumber + " to the power of " + powerOf + " = "+ firstNumber ** powerOf);
   } else if (powerOrSquare == "b") {
     var squareRootNumber = parseInt(prompt("Enter the number you would like to square root"));
-    var squareOfNumber = alert(Math.sqrt(squareRootNumber));
+    var squareOfNumber = alert("The square root of " + squareRootNumber + " = " + Math.sqrt(squareRootNumber));
   } else alert("Please enter type of calculator")
 }
